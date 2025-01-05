ec3a7140e18ded2ea1629beaec22959a758447b3
Viks313 <saanavikas.3@gmail.com>
2025-01-04 23:08:57 -0600

    [1] [Time Beats: 98.94%] [Memory Beats: 7.27%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> sums =new HashMap();
 
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement= target-nums[i];
 
-        for (int i = 0; i < nums.length; i++) {
-            
-            if (pairIdx.containsKey(target - nums[i])) {
-                return new int[] { i, pairIdx.get(target - nums[i]) };
+            if(sums.containsKey(complement)){
+                return new int[]{sums.get(complement),i};
             }
-            pairIdx.put(nums[i], i);
+            sums.put(nums[i],i);
         }
-
-        return new int[] {};
-
-        }
- }
+        return new int[]{};
+    }
+}
